0ccb21a1332dd7ca73b3c664e7a20a3e908d2430
polydome <polydome@outlook.com>
2020-08-07 15:32:24 +0200

    Create simple main activity

---
+++ app/src/main/java/com/github/polydome/apteczka/MainActivity.java
@@ -0,0 +1,9 @@
+package com.github.polydome.apteczka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
+}
