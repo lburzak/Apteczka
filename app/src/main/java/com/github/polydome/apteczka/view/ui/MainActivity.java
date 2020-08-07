191748ed52701f836dcfa04a4df1939d97deddf0
polydome <polydome@outlook.com>
2020-08-07 15:33:39 +0200

    Move main activity to `ui` package

--- app/src/main/java/com/github/polydome/apteczka/view/ui/MainActivity.java
+++ app/src/main/java/com/github/polydome/apteczka/view/ui/MainActivity.java
@@ -1,6 +1,8 @@
-package com.github.polydome.apteczka;
+package com.github.polydome.apteczka.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.polydome.apteczka.R;
 
 public class MainActivity extends AppCompatActivity {
     public MainActivity() {
