359afee50a225ea4625eb5a3a3e232cd2b330e94
polydome <polydome@outlook.com>
2020-08-08 10:34:07 +0200

    Cleanup

--- app/src/main/java/com/github/polydome/apteczka/di/component/PresentationComponent.java
+++ app/src/main/java/com/github/polydome/apteczka/di/component/PresentationComponent.java
@@ -1,8 +1,5 @@
 package com.github.polydome.apteczka.di.component;
 
-import com.github.polydome.apteczka.data.di.DataModule;
-import com.github.polydome.apteczka.di.module.ApplicationModule;
-import com.github.polydome.apteczka.di.module.DomainModule;
 import com.github.polydome.apteczka.di.module.PresentationModule;
 import com.github.polydome.apteczka.view.ui.EanInputDialog;
 
