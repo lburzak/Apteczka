fc043ae66b8c9b59de5f61fe6d263223b0a1ccdc
polydome <polydome@outlook.com>
2020-08-08 10:53:29 +0200

    Create EditMedicine activity layout

--- app/src/main/java/com/github/polydome/apteczka/view/ui/EditMedicineActivity.java
+++ app/src/main/java/com/github/polydome/apteczka/view/ui/EditMedicineActivity.java
@@ -2,6 +2,10 @@ package com.github.polydome.apteczka.view.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EditMedicineActivity extends AppCompatActivity {
+import com.github.polydome.apteczka.R;
 
+public class EditMedicineActivity extends AppCompatActivity {
+    public EditMedicineActivity() {
+        super(R.layout.activity_edit_medicine);
+    }
 }
